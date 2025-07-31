8406d40a1ac5d78998648db81eb1c053f1ede47c
keshew <artyom.korotckov2017@yandex.ru>
2025-08-01 01:50:18 +0500

    Initial Commit

---
+++ Stak/StakApp.swift
@@ -0,0 +1,17 @@
+//
+//  StakApp.swift
+//  Stak
+//
+//  Created by Артём Коротков on 01.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct StakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
